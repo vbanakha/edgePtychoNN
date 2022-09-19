068647239d296b7c361169336835831eee8f12fa
Anakha V Babu <vbanakha@gmail.com>
2022-09-19 14:13:54 -0400

    Delete framePreProcess.py

--- framePreProcess.py
+++
@@ -1,7 +0,0 @@
-from skimage.transform import resize
-import numpy as np 
-
-def frame_preprocess(frame, uid):
-    resize_frame=(frame[2:-2,2:-2].reshape(128,4,128,4).mean(3).mean(1).astype('float32'))
-    return resize_frame, uid
-
